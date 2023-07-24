475f17f5c5d54bb5a3ca7ae5f6295cb2b3e95910
ijazdev101 <ijazkhan19932@gmail.com>
2023-07-24 17:22:51 +0500

    Templates, Tags, Admin
    Templates, Tags, Admin

--- 1. Django Project setup/secondApp/views.py
+++ 1. Django Project setup/secondApp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+def home2(request):
+    return HttpResponse("Hello django home 2  page ")

26f1aada01ade6c90fcb8b6bdd899807471d5786
ijazdev101 <ijazkhan19932@gmail.com>
2023-07-29 09:43:05 +0500

    django
    django

---
+++ 2.Matrimonial_App/MatrimonialApp/matrimony/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from .models import Profile
+
+# Register your models here.
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'age', 'gender', 'occupation', 'email')
+    list_display_links = ('name', 'gender', 'email')
+    list_filter = ('gender', 'is_married')
+    search_fields = ('occupation',)
+
+
+admin.site.register(Profile, ProfileAdmin)
+
+# admin.site.register(Profile)
